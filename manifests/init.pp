eac5b098d763c1dc20b0a2fda4b89d08b1b14d53
mh <mh@immerda.ch>
2014-08-31 12:43:19 +0200

    add scl 1.1 from SL

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,4 @@
-# scl the new cloud way
+# scl for EL6
 class scl {
   package{'centos-release-SCL':
     ensure => present,
@@ -7,5 +7,22 @@ class scl {
   file{'/etc/yum.repos.d/CentOS-SCL.repo':
     ensure  => present,
     require => Package['centos-release-SCL'],
+  }
+
+  # SCLs are currently a little bit in a blocked state for official centos.
+  # That's why we use SL for the time being for SCL 1.1
+  # http://lists.centos.org/pipermail/centos-devel/2014-August/011772.html
+  file{'/etc/pki/rpm-gpg/RPM-GPG-KEY-sl6':
+    source => 'puppet:///modules/scl/gpgkeys/RPM-GPG-KEY-sl6',
+    owner  => root,
+    group  => 0,
+    mode   => '0644';
+  } -> yum::managed_yumrepo{
+    'softwarecollections':
+      descr    => 'Scientific Linux Software Collections - $basearch',
+      baseurl  => 'http://ftp.scientificlinux.org/linux/scientific/6x/external_products/softwarecollections/$basearch/',
+      enabled  => 1,
+      priority => 5,
+      gpgkey   => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-sl6'
   }
 }
