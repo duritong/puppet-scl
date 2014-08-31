c63f7ef75680435a66480b43df41c2bff467555e
mh <mh@immerda.ch>
2014-08-31 13:08:41 +0200

    add more extensions

--- manifests/php54.pp
+++ manifests/php54.pp
@@ -2,7 +2,14 @@
 class scl::php54 {
   require ::scl
 
-  package{
+  # we need some mor extensions
+  yum::managed_yumrepo{
+    'remi-php54more-epel-6-x86_64':
+      descr    => 'Php54more - epel-6-x86_64',
+      baseurl  => 'https://www.softwarecollections.org/repos/remi/php54more/epel-6-x86_64',
+      enabled  => 1,
+      gpgcheck => 0,
+  } -> package{
     [ 'php54-php-common',
       'php54-php-bcmath',
       'php54-php-cli',
@@ -13,15 +20,32 @@ class scl::php54 {
       'php54-php-imap',
       'php54-php-intl',
       'php54-php-mbstring',
+      'php54-php-mcrypt',
       'php54-php-mysqlnd',
       'php54-php-odbc',
       'php54-php-pdo',
       'php54-php-pear',
       'php54-php-pecl-apc',
+      'php54-php-pecl-crypto',
+      'php54-php-pecl-geoip',
+      'php54-php-pecl-gnupg',
+      'php54-php-pecl-imagick',
+      'php54-php-pecl-mailparse',
       'php54-php-pecl-memcache',
+      'php54-php-pecl-msgpack',
+      'php54-php-pecl-protocolbuffers',
+      'php54-php-pecl-quickhash',
+      'php54-php-pecl-scrypt',
+      'php54-php-pecl-selinux',
+      'php54-php-pecl-sqlite',
+      'php54-php-pecl-ssh2',
+      'php54-php-pecl-uploadprogress',
+      'php54-php-pecl-uuid',
+      'php54-php-pecl-yaml',
       'php54-php-pgsql',
       'php54-php-process',
       'php54-php-recode',
+      'php54-php-suhosin',
       'php54-php-soap',
       'php54-php-tidy',
       'php54-php-xml',
