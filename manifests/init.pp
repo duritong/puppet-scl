1e3ff96305a98d04c55c603f78d0cb89a3829113
mh <mh@immerda.ch>
2016-01-17 17:42:38 +0100

    this is not really anymore the case

--- manifests/init.pp
+++ manifests/init.pp
@@ -8,17 +8,4 @@ class scl {
     ensure  => present,
     require => Package['centos-release-SCL'],
   }
-
-  # SCLs are currently a little bit in a blocked state for official centos.
-  # That's why we use SL for the time being for SCL 1.1
-  # http://lists.centos.org/pipermail/centos-devel/2014-August/011772.html
-  yum::repo{
-    'softwarecollections':
-      descr         => 'Scientific Linux Software Collections - $basearch',
-      baseurl       => 'http://ftp.scientificlinux.org/linux/scientific/6x/external_products/softwarecollections/$basearch/',
-      enabled       => 1,
-      priority      => 5,
-      gpgkey        => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-sl6',
-      gpgkey_source => 'puppet:///modules/scl/gpgkeys/RPM-GPG-KEY-sl6',
-  }
 }
