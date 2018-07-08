36056fbf2862889b12e96b364e06550e05ca0ea4
mh <mh@immerda.ch>
2018-07-08 20:15:10 +0200

    add ruby25

---
+++ manifests/ruby25.pp
@@ -0,0 +1,10 @@
+# install ruby 2.5 incl. bundler
+class scl::ruby25 {
+  require ::scl
+
+  package{
+    [ 'rh-ruby25-ruby-devel',
+      'rh-ruby25-rubygem-bundler' ]:
+        ensure => present;
+  }
+}
---
+++ spec/classes/ruby25_spec.rb
@@ -0,0 +1,20 @@
+require File.expand_path(File.join(File.dirname(__FILE__),'../spec_helper'))
+
+describe 'scl::ruby25', :type => 'class' do
+  describe 'with standard' do
+    let(:facts) {
+      {
+        :operatingsystemmajrelease => '7',
+      }
+    }
+    let(:pre_condition){ 'include yum::prerequisites' }
+    it { is_expected.to compile.with_all_deps }
+    it { is_expected.to contain_class('scl') }
+    it { is_expected.to contain_package('rh-ruby25-ruby-devel').with(
+      :ensure => 'present',
+    )}
+    it { is_expected.to contain_package('rh-ruby25-rubygem-bundler').with(
+      :ensure => 'present',
+    )}
+  end
+end
