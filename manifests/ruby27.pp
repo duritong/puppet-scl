218614e8caf0b27a24dc030a812f8ba86fe0f63d
mh <mh@immerda.ch>
2020-11-30 00:08:26 +0100

    add ruby 2.7

---
+++ manifests/ruby27.pp
@@ -0,0 +1,10 @@
+# install ruby 2.7 incl. bundler
+class scl::ruby27 {
+  require ::scl
+
+  package{
+    [ 'rh-ruby27-ruby-devel',
+      'rh-ruby27-rubygem-bundler' ]:
+        ensure => present;
+  }
+}
