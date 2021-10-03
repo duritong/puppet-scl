63337c8c4260d4a3897ef932e3981fbda11c87b2
o <o@immerda.ch>
2021-10-03 21:32:12 +0200

    disable scl package handling for centos8

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,15 +1,17 @@
 # scl for centos
 class scl {
-  package { 'centos-release-SCL':
-    ensure => absent,
-  } -> package { 'centos-release-scl':
-    ensure => present,
-  }
-  # don't cleanup the repo
-  file { ['/etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-SIG-SCLo',
-      '/etc/yum.repos.d/CentOS-SCLo-scl.repo',
-    '/etc/yum.repos.d/CentOS-SCLo-scl-rh.repo',]:
-      ensure  => file,
-      require => Package['centos-release-scl'],
+  if versioncmp($facts['os']['release']['major'], '8') < 0 {
+    package { 'centos-release-SCL':
+      ensure => absent,
+    } -> package { 'centos-release-scl':
+      ensure => present,
+    }
+    # don't cleanup the repo
+    file { ['/etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-SIG-SCLo',
+        '/etc/yum.repos.d/CentOS-SCLo-scl.repo',
+      '/etc/yum.repos.d/CentOS-SCLo-scl-rh.repo',]:
+        ensure  => file,
+        require => Package['centos-release-scl'],
+    }
   }
 }
