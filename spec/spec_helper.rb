d792f7dbff2d8564c530fcb4c907bd4cb9840798
mh <mh@immerda.ch>
2024-04-30 21:36:12 +0200

    make tests run on ruby 3.3 and latest puppet

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,7 +5,6 @@ fixture_path = File.expand_path(File.join(__FILE__, '..', 'fixtures'))
 
 RSpec.configure do |c|
   c.module_path = File.join(fixture_path, 'modules')
-  c.manifest_dir = File.join(fixture_path, 'manifests')
   c.pattern = FileList[c.pattern].exclude(/^spec\/fixtures/)
 end
 
